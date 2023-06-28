d2eccd119f49bf29cda69657866e99efb36a5f56
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-06-28 17:45:39 +0200

    Add files via upload

---
+++ PalindromeWord/PalindromeWord/Program.cs
@@ -0,0 +1,16 @@
+﻿string s, revs = "";
+Console.WriteLine(" Insert the text");
+s = Console.ReadLine();
+for (int i = s.Length - 1; i >= 0; i--) 
+{
+    revs += s[i].ToString();
+}
+if (revs == s)
+{
+    Console.WriteLine("String is Palindrome ", s, revs);
+}
+else
+{
+    Console.WriteLine("String is not Palindrome ", s, revs);
+}
+Console.ReadKey();
