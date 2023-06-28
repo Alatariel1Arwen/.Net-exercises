e3c6f1bb91321ca036711ce7fb47206446f8cd60
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-06-28 17:49:26 +0200

    Add files via upload

---
+++ AccessDenied/AccessDenied/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessDenied
+{
+    internal class Class1
+    {
+        private int priv = 3;
+
+        protected int prot = 3;
+
+        internal int Inter = 4;
+
+        public int Pub = 5;
+
+
+        public int GetPriv()
+        {
+            return priv;
+        }
+        public int GetProt()
+        {
+            return prot;
+        }
+
+    }
+}
---
+++ AccessDenied/AccessDenied/Program.cs
@@ -0,0 +1,8 @@
+﻿using AccessDenied;
+
+Class1 class1 = new();
+
+Console.WriteLine( class1.Pub);
+Console.WriteLine(class1.Inter);
+Console.WriteLine(class1.GetPriv);
+Console.WriteLine(class1.GetProt);
