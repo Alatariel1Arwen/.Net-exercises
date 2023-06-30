faedc42e24344e639392ac057b4f0d0313c45dcd
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-06-30 15:27:52 +0200

    Add files via upload

---
+++ UnitConversion/UnitConversion/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the temperature in degrees Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        double fahrenheit = celsius * 1.8 + 32;
+
+        Console.WriteLine("The temperature in degrees Fahrenheit is: " + fahrenheit);
+        Console.ReadKey();
+    }
+}
