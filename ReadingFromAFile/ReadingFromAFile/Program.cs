6fd680855d6fb5e3f661f220dc7b46eab1b6ce0f
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-06-30 21:36:51 +0200

    Add files via upload

---
+++ ReadingFromAFile/ReadingFromAFile/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the path to the file:");
+        string percorsoFile = Console.ReadLine();
+
+        if (File.Exists(percorsoFile))
+        {
+            try
+        {
+                string contenutoFile = File.ReadAllText(percorsoFile);
+                Console.WriteLine("File content:");
+                Console.WriteLine(contenutoFile);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+        }
+        }
+        else
+        {
+            Console.WriteLine("The specified file does not exist.");
+        }
+
+       
+        Console.ReadLine();
+
+    }
+    
+}
