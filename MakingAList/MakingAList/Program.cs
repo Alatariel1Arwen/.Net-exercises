1aa7dafae20b4cb6e07ce2580b2c1ed973c2d7b0
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-06-30 16:38:07 +0200

    Add files via upload

---
+++ MakingAList/MakingAList/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> inputList = new List<string>();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("enter a value: ");
+    string input = Console.ReadLine();
+    inputList.Add(input);
+}
+
+Console.WriteLine("\nList from first to last:");
+foreach (string item in inputList)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("\nList from last to first:");
+for (int i = inputList.Count - 1; i >= 0; i--)
+{
+    Console.WriteLine(inputList[i]);
+}
