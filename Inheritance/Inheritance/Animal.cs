8f7869dd4205f6c5f155cfbaf403a37da24cba8f
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-06-30 16:07:46 +0200

    Add files via upload

---
+++ Inheritance/Inheritance/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public virtual void Speak() 
+        {
+            Console.WriteLine("Generic animal sound.");
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Inheritance;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Name = "Ciro";
+        dog.Age = "3";
+        dog.Speak();
+
+        Cat cat = new Cat();
+        cat.Name = "Immobile";
+        cat.Age = "6";
+        cat.Speak();
+
+        Horse horse = new Horse();
+        horse.Name = "Artex";
+        horse.Age = "9";
+        horse.Speak();
+
+    }
+    
+
+}
