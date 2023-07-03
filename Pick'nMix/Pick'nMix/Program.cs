200ba8bd0774101caaff8e36b9d3b190527447cd
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-03 10:30:40 +0200

    Add files via upload

---
+++ Pick'nMix/Pick'nMix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList myList = new ArrayList();
+
+        myList.Add(7);
+        myList.Add("Hello");
+        myList.Add(3.14);
+        myList.Add("World");
+
+        foreach (var lista in myList)
+        {
+            Console.WriteLine(lista);
+        }
+
+        Console.ReadLine();
+    }
+}
