a925de8dee0bc696bc5d0ba5e1a42f85b6cefd24
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-05 11:47:02 +0200

    Add files via upload

---
+++ MinAndMax/MinAndMax/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MinAndMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            List<int> numeriCasuali = new List<int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                numeriCasuali.Add(random.Next(1, 1000));
+            }
+
+            int min = numeriCasuali[0];
+            int max = numeriCasuali[0];
+
+            foreach (int numero in numeriCasuali)
+            {
+                if (numero < min)
+                {
+                    min = numero;
+                }
+
+                if (numero > max)
+                {
+                    max = numero;
+                }
+            }
+
+            Console.WriteLine("Random numbers generated:");
+            Console.WriteLine(string.Join(", ", numeriCasuali));
+            Console.WriteLine("Min number: " + min);
+            Console.WriteLine("Max number: " + max);
+        }
+    }
+}
