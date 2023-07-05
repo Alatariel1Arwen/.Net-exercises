62eca9986c0d43f8fd6c7a67909c007672884973
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-05 18:11:30 +0200

    Add files via upload

---
+++ QueryAListOfIntegers/QueryAListOfIntegers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            numbers.Add(random.Next(1, 1000));
+        }
+
+        var oddNumbers = from number in numbers
+                         where number % 2 != 0
+                         select number;
+
+        foreach (var number in oddNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
