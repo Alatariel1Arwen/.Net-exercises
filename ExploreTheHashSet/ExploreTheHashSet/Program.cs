b480c2e280c3561391f35c6c77a2b5a4e238cab5
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-05 11:31:04 +0200

    Add files via upload

---
+++ ExploreTheHashSet/ExploreTheHashSet/Program.cs
@@ -0,0 +1,19 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> uniqueStrings = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        uniqueStrings.Add("Apple");
+        uniqueStrings.Add("apple");
+        uniqueStrings.Add("Banana");
+        uniqueStrings.Add("banana");
+
+        Console.WriteLine("Elements nel HashSet:");
+        foreach (string str in uniqueStrings)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
