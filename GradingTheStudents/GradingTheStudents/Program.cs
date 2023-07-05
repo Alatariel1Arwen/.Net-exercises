661921498c504386a135d748296283eef65fdf12
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-05 12:05:54 +0200

    Add files via upload

---
+++ GradingTheStudents/GradingTheStudents/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using GradingTheStudents;
+
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>
+        {
+            new Student("Rachel", 70),
+            new Student("Ross", 90),
+            new Student("Monica", 99),
+            new Student("Chandler", 88),
+            new Student("Joey", 65),
+            new Student("Phoebe", 76),
+            new Student("Mike", 95),
+            new Student("Gunther", 80),
+            new Student("janice", 77),
+            new Student("Ben", 87)
+        };
+
+        double sumGrades = 0;
+        foreach (var student in students)
+        {
+            sumGrades += student.Grade;
+        }
+        double averageGrade = sumGrades / students.Count;
+
+        Console.WriteLine("The average grade of the students is: " + averageGrade);
+
+        Student studentWithHighestGrade = null;
+        int highestGrade = -1;
+        foreach (var student in students)
+        {
+            if (student.Grade > highestGrade)
+            {
+                highestGrade = student.Grade;
+                studentWithHighestGrade = student;
+            }
+        }
+
+        Console.WriteLine("The student with the highest grade is: " + studentWithHighestGrade.Name + " (" + studentWithHighestGrade.Grade + ")");
+    }
+}
---
+++ GradingTheStudents/GradingTheStudents/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace GradingTheStudents
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+
+        public Student(string name, int grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+    }
+}
