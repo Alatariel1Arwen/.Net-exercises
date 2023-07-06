e2e6b3f313db4ad5444ef0c0c6971aff40b485e2
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-06 14:53:15 +0200

    Add files via upload

---
+++ ThatSumsItUp/ThatSumsItUp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<double> numbers = new List<double> { 1.2, 2.3, 3.4, 4.5, 5.6 };
+
+        double sum = numbers.Sum();
+
+        double average = numbers.Average();
+
+        int count = numbers.Count;
+
+        Console.WriteLine("Elenco numeri:");
+
+        foreach (double number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Somma: " + sum);
+        Console.WriteLine("Media: " + average);
+        Console.WriteLine("Numero totale di elementi: " + count);
+    }
+}
