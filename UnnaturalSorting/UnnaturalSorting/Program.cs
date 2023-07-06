b3bae7a2a4694604f78709e9fa1421380d529a19
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-06 14:40:51 +0200

    Add files via upload

---
+++ UnnaturalSorting/UnnaturalSorting/Program.cs
@@ -0,0 +1,21 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            numbers.Add(random.Next(1, 1000));
+        }
+
+        var sortedNumbers = numbers.OrderBy(n => n.ToString().Length);
+
+        foreach (var number in sortedNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
