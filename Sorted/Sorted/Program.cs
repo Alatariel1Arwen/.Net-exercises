f6079ff92b8609382822819da706c7bd7a53f475
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-06 14:49:33 +0200

    Add files via upload

---
+++ Sorted/Sorted/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        int[] numbers = Enumerable.Range(1, 1000).Select(x => random.Next()).ToArray();
+
+        var ascendingOrder = numbers.OrderBy(x => x);
+        var descendingOrder = numbers.OrderByDescending(x => x);
+
+        Console.WriteLine("Ascending order:");
+
+        foreach (int number in ascendingOrder)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Descending order:");
+
+        foreach (int number in descendingOrder)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
