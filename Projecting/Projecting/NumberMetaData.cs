55366c729a8d7470c11aa79359bcee6f92a28392
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-17 11:44:27 +0200

    Add files via upload

---
+++ Projecting/Projecting/NumberMetaData.cs
@@ -0,0 +1,16 @@
+﻿namespace Projecting
+{
+    class NumberMetaData
+    {
+        public int Number { get; }
+        public bool IsEven { get; }
+        public int NumberOfCharacters { get; }
+
+        public NumberMetaData(int number)
+        {
+            Number = number;
+            IsEven = number % 2 == 0;
+            NumberOfCharacters = number.ToString().Length;
+        }
+    }
+}
---
+++ Projecting/Projecting/Program.cs
@@ -0,0 +1,17 @@
+﻿using Projecting;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        List<int> numbers = Enumerable.Range(1, 100).Select(_ => random.Next(1, 1000)).ToList();
+
+        List<NumberMetaData> metaDataList = numbers.Select(n => new NumberMetaData(n)).ToList();
+
+        foreach (NumberMetaData metaData in metaDataList)
+        {
+            Console.WriteLine($"Number: {metaData.Number}, IsEven: {metaData.IsEven}, NumberOfCharacters: {metaData.NumberOfCharacters}");
+        }
+    }
+}
