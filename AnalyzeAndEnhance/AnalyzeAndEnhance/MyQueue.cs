a95543b6a1d29dd1dd55cd8d7355dd211491e5a1
Michela <104628807+Alatariel1Arwen@users.noreply.github.com>
2023-07-18 18:17:43 +0200

    Add files via upload

---
+++ AnalyzeAndEnhance/AnalyzeAndEnhance/MyQueue.cs
@@ -0,0 +1,46 @@
+﻿
+namespace LetsCreateAQueue
+{
+    public class MyQueue<T>
+    {
+        public T[] array;
+        private int firstElement;
+
+        public MyQueue(int capacity) 
+        {
+            array = new T[capacity];
+            firstElement = -1;
+        }
+
+        public void Enqueue(T value)
+        {
+            if (firstElement == array.Length -1)
+            {
+                throw new InvalidOperationException("The queue is full.");
+            }
+            array[++firstElement] = value;
+        }
+
+        public T Dequeue()
+        {
+            if (firstElement == -1)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+            T item = array[0];
+            Array.Copy(array, 1, array, 0, firstElement);
+            array[firstElement--] = default(T);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (firstElement == -1)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+
+            return array[0];
+        }
+    }
+}
---
+++ AnalyzeAndEnhance/AnalyzeAndEnhance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using LetsCreateAQueue;
+
+   var myQueueInteger = new MyQueue<int>(10);
+
+    myQueueInteger.Enqueue(1);
+    myQueueInteger.Enqueue(2);
+    myQueueInteger.Enqueue(3);
+    myQueueInteger.Enqueue(4);
+    myQueueInteger.Enqueue(5);
+    myQueueInteger.Enqueue(6);
+
+int dequeuedItem = myQueueInteger.Dequeue();
+Console.WriteLine(dequeuedItem);
+
+int peekedItem = myQueueInteger.Peek();
+Console.WriteLine(peekedItem);
+
+
+var myQueueString= new MyQueue<string>(10);
+
+myQueueString.Enqueue("Monica");
+myQueueString.Enqueue("Chandler");
+myQueueString.Enqueue("Ross");
+myQueueString.Enqueue("Rachel");
+myQueueString.Enqueue("Joey");
+myQueueString.Enqueue("Phoebe");
+
+string dequeueString = myQueueString.Dequeue();
+Console.WriteLine(dequeueString);
+
+string peekedString = myQueueString.Peek();
+Console.WriteLine(peekedString);
+
+
+
+try
+{
+
+
+    try
+    {
+     throw new Exception();
+    }
+    catch (Exception e)
+    {
+    throw e;
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.StackTrace);
+}
+   
